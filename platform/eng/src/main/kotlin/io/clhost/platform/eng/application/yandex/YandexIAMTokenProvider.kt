ffc990c968fbb7a1f79fbc93ed21c687d310007a
clhost <clhost8@yandex.ru>
2023-08-27 13:47:03 +0200

    IAM token refreshment becomes more modern

--- platform/eng/src/main/kotlin/io/clhost/platform/eng/application/yandex/YandexIAMTokenProvider.kt
+++ platform/eng/src/main/kotlin/io/clhost/platform/eng/application/yandex/YandexIAMTokenProvider.kt
@@ -1,11 +1,15 @@
 package io.clhost.platform.eng.application.yandex
 
+import io.clhost.extension.jackson.jsonDecode
+import io.clhost.extension.jackson.jsonEncode
 import io.clhost.extension.ktor.client.applicationJson
 import io.clhost.extension.ktor.client.plugin.label
+import io.clhost.extension.stdlib.withResult
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
+import java.time.OffsetDateTime
 import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
@@ -39,8 +43,23 @@ class YandexIAMTokenProvider(
 
     @Scheduled(initialDelay = 1_000, fixedDelay = 60_000_000)
     fun refreshToken() {
-        redisTemplate.opsForValue()[iamTokenKey] = getNewIAMToken()
-        logger.info("Successfully refreshed token")
+        val result = withResult { getIAMToken() }
+
+        if (result.isFailure) {
+            redisTemplate.opsForValue()[iamTokenKey] = jsonEncode(getNewIAMToken().toIAMToken())
+            logger.info("Successfully set new IAM token")
+            return
+        }
+
+        val iamToken = jsonDecode<IAMToken>(result.getOrThrow())
+
+        if (OffsetDateTime.now() > iamToken.expiredAt) {
+            redisTemplate.opsForValue()[iamTokenKey] = jsonEncode(getNewIAMToken().toIAMToken())
+            logger.info("Successfully refreshed IAM token")
+            return
+        }
+
+        logger.info("There is no need to refresh IAM token")
     }
 
     private fun getNewIAMToken() = runBlocking {
@@ -48,9 +67,12 @@ class YandexIAMTokenProvider(
             label("getNewIAMToken")
             applicationJson()
             setBody(Exchange(oauthToken))
-        }.body<IAMToken>().iamToken
+        }.body<IAMTokenResponse>()
     }
 
-    data class IAMToken(val iamToken: String)
-    data class Exchange(val yandexPassportOauthToken: String)
+    private data class IAMToken(val iamToken: String, val expiredAt: OffsetDateTime)
+    private data class IAMTokenResponse(val iamToken: String, val expiresAt: OffsetDateTime)
+    private data class Exchange(val yandexPassportOauthToken: String)
+
+    private fun IAMTokenResponse.toIAMToken() = IAMToken(iamToken, OffsetDateTime.now().plusHours(1))
 }
